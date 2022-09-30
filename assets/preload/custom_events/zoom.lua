10f5e7e2a4df66e03735788ff1766c10a94c0410
FlorkGames <105853494+FlorkGames@users.noreply.github.com>
2022-09-30 09:25:10 -0300

    Add files via upload

---
+++ assets/preload/custom_events/zoom.lua
@@ -0,0 +1,105 @@
+local xx = 420.95;
+local yy = 513;
+local xx2 = 952.9;
+local yy2 = 550;
+local ofs = 60;
+local followchars = true;
+local del = 0;
+local del2 = 0;
+
+local xx3 = 600;
+local yy3 = 100;
+
+function onUpdate()
+    if followchars == true then
+        if mustHitSection == false then
+            if gfSection == true then
+                setProperty('defaultCamZoom',0.5)
+                if getProperty('gf.animation.curAnim.name') == 'singLEFT' then
+                    triggerEvent('Camera Follow Pos',xx3-ofs,yy3)
+                end
+                if getProperty('gf.animation.curAnim.name') == 'singRIGHT' then
+                    triggerEvent('Camera Follow Pos',xx3+ofs,yy3)
+                end
+                if getProperty('gf.animation.curAnim.name') == 'singUP' then
+                    triggerEvent('Camera Follow Pos',xx3,yy3-ofs)
+                end
+                if getProperty('gf.animation.curAnim.name') == 'singDOWN' then
+                    triggerEvent('Camera Follow Pos',xx3,yy3+ofs)
+                end
+                if getProperty('gf.animation.curAnim.name') == 'singLEFT-alt' then
+                    triggerEvent('Camera Follow Pos',xx3-ofs,yy3)
+                end
+                if getProperty('gf.animation.curAnim.name') == 'singRIGHT-alt' then
+                    triggerEvent('Camera Follow Pos',xx3+ofs,yy3)
+                end
+                if getProperty('gf.animation.curAnim.name') == 'singUP-alt' then
+                    triggerEvent('Camera Follow Pos',xx3,yy3-ofs)
+                end
+                if getProperty('gf.animation.curAnim.name') == 'singDOWN-alt' then
+                    triggerEvent('Camera Follow Pos',xx3,yy3+ofs)
+                end
+                if getProperty('gf.animation.curAnim.name') == 'idle-alt' then
+                    triggerEvent('Camera Follow Pos',xx3,yy3)
+                end
+                if getProperty('gf.animation.curAnim.name') == 'idle' then
+                    triggerEvent('Camera Follow Pos',xx3,yy3)
+                end
+            else
+                setProperty('defaultCamZoom',0.8)
+                if getProperty('dad.animation.curAnim.name') == 'singLEFT' then
+                    triggerEvent('Camera Follow Pos',xx-ofs,yy)
+                end
+                if getProperty('dad.animation.curAnim.name') == 'singRIGHT' then
+                    triggerEvent('Camera Follow Pos',xx+ofs,yy)
+                end
+                if getProperty('dad.animation.curAnim.name') == 'singUP' then
+                    triggerEvent('Camera Follow Pos',xx,yy-ofs)
+                end
+                if getProperty('dad.animation.curAnim.name') == 'singDOWN' then
+                    triggerEvent('Camera Follow Pos',xx,yy+ofs)
+                end
+                if getProperty('dad.animation.curAnim.name') == 'singLEFT-alt' then
+                    triggerEvent('Camera Follow Pos',xx-ofs,yy)
+                end
+                if getProperty('dad.animation.curAnim.name') == 'singRIGHT-alt' then
+                    triggerEvent('Camera Follow Pos',xx+ofs,yy)
+                end
+                if getProperty('dad.animation.curAnim.name') == 'singUP-alt' then
+                    triggerEvent('Camera Follow Pos',xx,yy-ofs)
+                end
+                if getProperty('dad.animation.curAnim.name') == 'singDOWN-alt' then
+                    triggerEvent('Camera Follow Pos',xx,yy+ofs)
+                end
+                if getProperty('dad.animation.curAnim.name') == 'idle-alt' then
+                    triggerEvent('Camera Follow Pos',xx,yy)
+                end
+                if getProperty('dad.animation.curAnim.name') == 'idle' then
+                    triggerEvent('Camera Follow Pos',xx,yy)
+                end
+            end
+        else
+            setProperty('defaultCamZoom',1.0)
+            if getProperty('boyfriend.animation.curAnim.name') == 'singLEFT' then
+                triggerEvent('Camera Follow Pos',xx2-ofs,yy2)
+            end
+            if getProperty('boyfriend.animation.curAnim.name') == 'singRIGHT' then
+                triggerEvent('Camera Follow Pos',xx2+ofs,yy2)
+            end
+            if getProperty('boyfriend.animation.curAnim.name') == 'singUP' then
+                triggerEvent('Camera Follow Pos',xx2,yy2-ofs)
+            end
+            if getProperty('boyfriend.animation.curAnim.name') == 'singDOWN' then
+                triggerEvent('Camera Follow Pos',xx2,yy2+ofs)
+            end
+            if getProperty('boyfriend.animation.curAnim.name') == 'idle-alt' then
+                triggerEvent('Camera Follow Pos',xx2,yy2)
+            end
+            if getProperty('boyfriend.animation.curAnim.name') == 'idle' then
+                triggerEvent('Camera Follow Pos',xx2,yy2)
+            end
+        end
+    else
+        triggerEvent('Camera Follow Pos','','')
+    end
+end
